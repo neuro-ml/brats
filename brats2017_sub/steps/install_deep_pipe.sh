f4e1d6ff8f89ee42248af0fd7c1cc89384be6c6d
Egor Krivov <egor.krivov@frtk.ru>
2017-10-06 17:33:16 +0300

    Update install_deep_pipe.sh

--- brats2017_sub/steps/install_deep_pipe.sh
+++ brats2017_sub/steps/install_deep_pipe.sh
@@ -1,3 +1,3 @@
 git clone --recursive https://github.com/neuro-ml/deep_pipe.git ../deep_pipe
 cd ../deep_pipe
-git checkout 7de8d7d4570d6a43c14f3d0675a326cc6fc6a794
+git checkout 87c1d6814cd2172375288deb17c077f17678f76d
